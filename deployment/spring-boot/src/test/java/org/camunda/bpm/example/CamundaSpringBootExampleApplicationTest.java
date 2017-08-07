e4d66ab5604c9714f194beb6fcff55fe0999eb5d
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-08-07 15:38:26 +0200

    Integrated the vPAV 2.0.0 to the spring boot example process

--- deployment/spring-boot/src/test/java/org/camunda/bpm/example/CamundaSpringBootExampleApplicationTest.java
+++ deployment/spring-boot/src/test/java/org/camunda/bpm/example/CamundaSpringBootExampleApplicationTest.java
@@ -3,6 +3,7 @@ package org.camunda.bpm.example;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
@@ -11,28 +12,40 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.viadee.bpm.vPAV.ProcessApplicationValidator;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CamundaSpringBootExampleApplication.class)
 public class CamundaSpringBootExampleApplicationTest {
 
-  @Autowired
-  private RuntimeService runtimeService;
+    @Autowired
+    private RuntimeService runtimeService;
 
-  @Autowired
-  private TaskService taskService;
+    @Autowired
+    private TaskService taskService;
 
-	@Test
-	public void verifyProcessInstanceStarted() {
-	  // process instance is started by the application and waits on the user task
-	  Task task = taskService.createTaskQuery().taskName("Approve Loan").singleResult();
-	  assertThat(task, is(notNullValue()));
+    @Test
+    public void verifyProcessInstanceStarted() {
+        // process instance is started by the application and waits on the user task
+        Task task = taskService.createTaskQuery().taskName("Approve Loan").singleResult();
+        assertThat(task, is(notNullValue()));
 
-	  // complete the user task and verify that the process ends
-	  taskService.complete(task.getId());
+        // complete the user task and verify that the process ends
+        taskService.complete(task.getId());
 
-	  assertThat(runtimeService.createProcessInstanceQuery().count(), is(0L));
-	}
+        assertThat(runtimeService.createProcessInstanceQuery().count(), is(0L));
+    }
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Test
+    public void validateModel() {
+        assertTrue("Model inconsistency found. Please check target folder for validation output",
+                ProcessApplicationValidator.findModelInconsistencies(ctx).isEmpty());
+    }
 
 }
