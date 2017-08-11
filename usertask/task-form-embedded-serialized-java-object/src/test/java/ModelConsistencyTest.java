7b099154b2f7dd9ca123b8da88bae6c1ba9de359
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-08-11 11:06:58 +0200

    Integrated the vPAV to the project

---
+++ usertask/task-form-embedded-serialized-java-object/src/test/java/ModelConsistencyTest.java
@@ -0,0 +1,15 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.viadee.bpm.vPAV.ProcessApplicationValidator;
+
+public class ModelConsistencyTest {
+
+    @Test
+    public void validateModel() {
+        assertTrue("Model inconsistency found. Please check target folder for validation output",
+                ProcessApplicationValidator.findModelInconsistencies().isEmpty());
+    }
+}
