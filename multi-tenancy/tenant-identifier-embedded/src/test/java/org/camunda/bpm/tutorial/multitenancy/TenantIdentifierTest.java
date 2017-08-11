2203cd7707d5f3fef277b24a8c95e79ab72a8f94
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-08-11 11:10:33 +0200

    Integrated the vPAV to the project

--- multi-tenancy/tenant-identifier-embedded/src/test/java/org/camunda/bpm/tutorial/multitenancy/TenantIdentifierTest.java
+++ multi-tenancy/tenant-identifier-embedded/src/test/java/org/camunda/bpm/tutorial/multitenancy/TenantIdentifierTest.java
@@ -1,20 +1,19 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.camunda.bpm.tutorial.multitenancy;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -28,90 +27,99 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import de.viadee.bpm.vPAV.ProcessApplicationValidator;
+
 public class TenantIdentifierTest {
 
-  @Rule
-  public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+    @Rule
+    public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
-  private RepositoryService repositoryService;
-  private RuntimeService runtimeService;
+    private RepositoryService repositoryService;
 
-  private Service mockService;
+    private RuntimeService runtimeService;
 
-  @Before
-  public void initServices() {
-    repositoryService = processEngineRule.getRepositoryService();
-    runtimeService = processEngineRule.getRuntimeService();
+    private Service mockService;
 
-    // create a mock service and register it on the service task
-    mockService = mock(Service.class);
-    TenantAwareServiceTask.setService(mockService);
-  }
+    @Before
+    public void initServices() {
+        repositoryService = processEngineRule.getRepositoryService();
+        runtimeService = processEngineRule.getRuntimeService();
 
-  @Test
-  public void deployProcessDefinitionsAndStartProcessInstances() {
-    // deploy process definition for 'tenant1'
-    repositoryService
-      .createDeployment()
-      .tenantId("tenant1")
-      .addClasspathResource("processes/tenant1/tenant1_process.bpmn")
-      .deploy();
-
-    // deploy another process definition for 'tenant2'
-    repositoryService
-      .createDeployment()
-      .tenantId("tenant2")
-      .addClasspathResource("processes/tenant2/tenant2_process.bpmn")
-      .deploy();
-
-    // check that two process definitions are deployed
-    assertThat(repositoryService.createProcessDefinitionQuery().count(), is(2L));
-
-    // start a process instance for 'tenant1'
-    runtimeService
-      .createProcessInstanceByKey("process")
-      .processDefinitionTenantId("tenant1")
-      .execute();
-
-    // check that the process instance have the same tenant-id
-    ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().singleResult();
-    assertThat(processInstance, is(notNullValue()));
-    assertThat(processInstance.getTenantId(), is("tenant1"));
-
-    // next, start a process instance for 'tenant2'
-    runtimeService
-      .createProcessInstanceByKey("process")
-      .processDefinitionTenantId("tenant2")
-      .execute();
-
-    // check the tenant-id of the process instance
-    processInstance = runtimeService.createProcessInstanceQuery().tenantIdIn("tenant2").singleResult();
-    assertThat(processInstance, is(notNullValue()));
-    assertThat(processInstance.getTenantId(), is("tenant2"));
-  }
-
-  @Test
-  public void tenantAwareServiceTask() {
-    // deploy process definition for 'tenant1'
-    repositoryService
-      .createDeployment()
-      .tenantId("tenant1")
-      .addClasspathResource("processes/tenant1/tenant1_process.bpmn")
-      .deploy();
-
-    // start a process instance by key
-    // - note that this would fail if the process definition is also deployed for 'tenant2'
-    runtimeService.startProcessInstanceByKey("process");
-
-    // check that the service was invoked for 'tenant1'
-    verify(mockService).invoke("tenant1");
-  }
-
-  @After
-  public void clean() {
-    for(Deployment deployment : repositoryService.createDeploymentQuery().list()) {
-      repositoryService.deleteDeployment(deployment.getId(), true);
+        // create a mock service and register it on the service task
+        mockService = mock(Service.class);
+        TenantAwareServiceTask.setService(mockService);
     }
-  }
+
+    @Test
+    public void deployProcessDefinitionsAndStartProcessInstances() {
+        // deploy process definition for 'tenant1'
+        repositoryService
+                .createDeployment()
+                .tenantId("tenant1")
+                .addClasspathResource("processes/tenant1/tenant1_process.bpmn")
+                .deploy();
+
+        // deploy another process definition for 'tenant2'
+        repositoryService
+                .createDeployment()
+                .tenantId("tenant2")
+                .addClasspathResource("processes/tenant2/tenant2_process.bpmn")
+                .deploy();
+
+        // check that two process definitions are deployed
+        assertThat(repositoryService.createProcessDefinitionQuery().count(), is(2L));
+
+        // start a process instance for 'tenant1'
+        runtimeService
+                .createProcessInstanceByKey("process")
+                .processDefinitionTenantId("tenant1")
+                .execute();
+
+        // check that the process instance have the same tenant-id
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().singleResult();
+        assertThat(processInstance, is(notNullValue()));
+        assertThat(processInstance.getTenantId(), is("tenant1"));
+
+        // next, start a process instance for 'tenant2'
+        runtimeService
+                .createProcessInstanceByKey("process")
+                .processDefinitionTenantId("tenant2")
+                .execute();
+
+        // check the tenant-id of the process instance
+        processInstance = runtimeService.createProcessInstanceQuery().tenantIdIn("tenant2").singleResult();
+        assertThat(processInstance, is(notNullValue()));
+        assertThat(processInstance.getTenantId(), is("tenant2"));
+    }
+
+    @Test
+    public void tenantAwareServiceTask() {
+        // deploy process definition for 'tenant1'
+        repositoryService
+                .createDeployment()
+                .tenantId("tenant1")
+                .addClasspathResource("processes/tenant1/tenant1_process.bpmn")
+                .deploy();
+
+        // start a process instance by key
+        // - note that this would fail if the process definition is also deployed for 'tenant2'
+        runtimeService.startProcessInstanceByKey("process");
+
+        // check that the service was invoked for 'tenant1'
+        verify(mockService).invoke("tenant1");
+    }
+
+    @After
+    public void clean() {
+        for (Deployment deployment : repositoryService.createDeploymentQuery().list()) {
+            repositoryService.deleteDeployment(deployment.getId(), true);
+        }
+    }
+
+    @Test
+    public void validateModel() {
+        assertTrue("Model inconsistency found. Please check target folder for validation output",
+                ProcessApplicationValidator.findModelInconsistencies().isEmpty());
+    }
 
 }
