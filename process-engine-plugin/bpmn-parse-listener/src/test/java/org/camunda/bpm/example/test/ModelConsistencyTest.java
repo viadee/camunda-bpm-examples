b03d8ac662f4546e0dd8f6fdd4dc81d3752ee98e
Sascha Di Bernardo <sascha.dibernardo@viadee.de>
2017-09-25 12:20:04 +0200

    vPAV-109

--- process-engine-plugin/bpmn-parse-listener/src/test/java/org/camunda/bpm/example/test/ModelConsistencyTest.java
+++ process-engine-plugin/bpmn-parse-listener/src/test/java/org/camunda/bpm/example/test/ModelConsistencyTest.java
@@ -3,25 +3,14 @@ package org.camunda.bpm.example.test;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.viadee.bpm.vPAV.ProcessApplicationValidator;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { SpringTestConfig.class })
-
 public class ModelConsistencyTest {
-
-    @Autowired
-    private ApplicationContext ctx;
 
     @Test
     public void validateModel() {
         assertTrue("Model inconsistency found. Please check target folder for validation output",
-                ProcessApplicationValidator.findModelInconsistencies(ctx).isEmpty());
+                ProcessApplicationValidator.findModelInconsistencies().isEmpty());
     }
 }
